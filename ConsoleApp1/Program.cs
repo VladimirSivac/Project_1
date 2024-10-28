96ac72af9d36cefe97687895b9a94ce2d7ce94a1
VladimirSivac <morndes@gmail.com>
2024-10-28 22:11:29 +0200

    Project_1
    Компьютерный практикум №4 (Задачи 1, 2, 3, 4)

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,13 +10,75 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            /* Задача 1 (целые числа)
             Console.Write("number1= ");
-            int number1=int.Parse(Console.ReadLine());
+            int number1 = int.Parse(Console.ReadLine());
+
             Console.Write("number2= ");
             int number2 = int.Parse(Console.ReadLine());
-            int sum=number1 + number2;
+
+            int sum = number1 + number2;
             Console.WriteLine($"sum= {sum}");
-            Console.Read();
+            Console.Read();*/
+
+            /* Задача 1 (вещественные числа)
+            Console.Write("number1= ");
+            double number1 = double.Parse(Console.ReadLine());
+
+            Console.Write("number2= ");
+            double number2 = double.Parse(Console.ReadLine());
+
+            double sum = number1 + number2;
+            Console.WriteLine($"sum= {sum}");
+            Console.Read();*/
+
+            /* Задача 2 (целый тип данных)
+            Console.Write("Длина прямоугольника a= ");
+            int a = int.Parse(Console.ReadLine());
+            
+            Console.Write("Ширина прямоугольника b= ");
+            int b = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"Площадь прямоугольника= {a * b}");
+            Console.WriteLine($"Периметр прямоугольника= {2 * (a + b)}");
+            Console.ReadLine();*/
+
+            /* Задача 2 (вещественный тип данных)
+            Console.Write("Длина прямоугольника a= ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Ширина прямоугольника b= ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Площадь прямоугольника= {a * b}");
+            Console.WriteLine($"Периметр прямоугольника= {2 * (a + b)}");
+            Console.ReadLine();*/
+
+            /* Задача 3
+            Console.Write("Первый катет a= ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Второй катет b= ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Гипотенуза= {Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2))}");
+            Console.ReadLine();*/
+
+            /* Задача 4 (целый тип данных)
+            Console.Write("Сторона квадрата a= ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Периметр квадрата= {a * 4}");
+            Console.WriteLine($"Площадь квадрата= {Math.Pow(a, 2)}");
+            Console.ReadLine();*/
+
+            /* Задача 4 (вещественный тип данных)
+            Console.Write("Сторона квадрата a= ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Периметр квадрата= {a * 4}");
+            Console.WriteLine($"Площадь квадрата= {Math.Pow(a, 2)}");
+            Console.ReadLine();*.
         }
     }
 }
